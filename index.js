b1ab2e4b127a114d84774218fa59dc8314e1f159
ashmit <ashmit.dudeja@gmail.com>
2025-07-17 00:39:00 +0530

    final  commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500'],
+  origin: ['https://mailstorn.onrender.com', 'http://127.0.0.1:5500', 'http://localhost:5500'],
   credentials: true
 }));
 
